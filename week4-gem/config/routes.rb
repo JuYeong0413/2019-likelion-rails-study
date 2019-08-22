c57dcc2038ac5886c5e396e275e0171cd0e106e6
JuYeong0413 <2jy22@naver.com>
2019-08-22 16:25:19 +0900

    week4 - 2019/07/24

---
+++ week4-gem/config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  devise_for :users
+  root 'posts#index'
+  resources :posts do
+    resources :comments
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
